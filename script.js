3e3a5fbe6fc8cab45725578802dba7b3d0e7e195
jinpas <124241488+jinpas@users.noreply.github.com>
2024-05-25 17:27:40 -0400

    Addition of comments

--- script.js
+++ script.js
@@ -1,96 +1,85 @@
+// Function to generate a random integer between 'lower' and 'upper' (inclusive)
 function getRandomInteger(lower, upper) {
-  //R = parseInt(rnd * (upper - (lower -1)) + lower
+  // Formula: R = parseInt(rnd * (upper - (lower - 1)) + lower)
   var multiplier = upper - (lower - 1);
   var rnd = parseInt(Math.random() * multiplier) + lower;
-
   return rnd;
 }
 
+// Function to roll the dice and execute necessary functions
 function rollDiceAndRun() {
   getresults(); // Call the getresults() function first
   run(); // Then call the run() function
   rotateDice(); // Rotate the dice every time it's clicked
 }
 
+// Function to retrieve input values and initiate result generation
 function getresults() {
   rolls = inputContainer.rolls.value;
   dicenum = inputContainer.dicenum.value;
   if (inputContainer.rolls.value == "" || inputContainer.dicenum.value == "") {
-    //if no value entered
-    alert(
-      "Please pick the number of dice and how many times you want to roll it to continue"
-    );
+    // If no value entered, show an alert
+    alert("Please pick the number of dice and how many times you want to roll it to continue");
   } else {
-    //else run the program to get results
+    // Otherwise, proceed with result generation
     numRolls = Number(rolls);
     numDice = Number(dicenum);
-    //sets the vaule of amount of rolls needed
-
     rolldie();
-
     fcleartable();
     median();
     mean();
-
     fdoubles();
     ftriples();
     fonetable();
     ftwotable();
     fthreetable();
-
     mode();
-
     empty();
   }
 }
 
+// Function to clear all rows in the stats table
 function fcleartable() {
   for (var i = 6; i < statsTable.rows.length; i + 1) {
     statsTable.deleteRow(i);
   }
-  //deletes all rows in the table
-  statsTable = document.getElementById("statstable");
 }
+
+// Function to empty the results arrays
 function empty() {
   results = [];
   sortedresults = [];
-  //empties the results
 }
 
+// Function to roll the dice and populate the results table
 function rolldie() {
+  // Add rolling animation to the dice
   document.getElementById("dice").className = "rolldice";
   for (var i = 0; i < numRolls; i++) {
     var diceRoll = getRandomInteger(1, 6);
-
     newRow = diceTable.insertRow();
     newCell = newRow.insertCell();
     newCell.innerHTML = "Roll " + (i + 1);
-
     newCell = newRow.insertCell();
     newCell.innerHTML = diceRoll;
     results.push(diceRoll);
     sortedresults.push(diceRoll);
-
     if (dicenum == 1) {
-      //adds two empty cells if only one die is rolled
       newCell = newRow.insertCell();
       newCell.innerHTML = " ";
       newCell = newRow.insertCell();
       newCell.innerHTML = " ";
     } else {
-      //adds a second cell otherwise because it will be for two or three dies
       var secondroll = getRandomInteger(1, 6);
       newCell = newRow.insertCell();
       newCell.innerHTML = secondroll;
       results.push(secondroll);
       sortedresults.push(secondroll);
       if (dicenum == 2) {
-        //if there are 2 dies, thent there would be a third empty cell
         newCell = newRow.insertCell();
         newCell.innerHTML = " ";
       }
       if (dicenum == 3) {
-        //if there are three dies, the third cell will be full
         var thirdroll = getRandomInteger(1, 6);
         newCell = newRow.insertCell();
         newCell.innerHTML = thirdroll;
@@ -99,9 +88,9 @@ function rolldie() {
       }
     }
   }
-  console.log("the value of results is " + results);
 }
 
+// Function to calculate the mean of dice rolls
 function mean() {
   var sum = 0;
   var mean = 0;
@@ -109,38 +98,28 @@ function mean() {
     sum += results[i];
   }
   mean = (sum / results.length).toFixed(2);
-  console.log("the value of the mean is " + mean);
-
   document.getElementById("mean").innerHTML = mean;
 }
+
+// Function to calculate the median of dice rolls
 function median() {
   var sorted = sortedresults.sort();
-  console.log("the value of the sorted is " + sorted);
-  if (
-    sorted.length % 2 ==
-    0
-    // is even
-  ) {
-    // average of two middle numbers
-    console.log("boo");
+  var med;
+  if (sorted.length % 2 == 0) {
     med = (sorted[sorted.length / 2 - 1] + sorted[sorted.length / 2]) / 2;
   } else {
-    // is odd
-    // middle number only
     med = sorted[(sorted.length - 1) / 2];
-    console.log("yay" + (sorted.length - 1) / 2);
   }
-  console.log("the value of the median is " + med);
   document.getElementById("median").innerHTML = med;
 }
+
+// Function to calculate the mode of dice rolls
 function mode() {
   var max = 0;
   modes = [];
-  //console.log("the frequencies are " + modesfreq);
   for (i = 1; i < modesfreq.length; i++) {
     if (modesfreq[i] > max) {
       max = modesfreq[i];
-      //console.log("the max is " + max);
     }
   }
   for (i = 1; i < modesfreq.length; i++) {
@@ -148,145 +127,107 @@ function mode() {
       modes.push(i);
     }
   }
-  //console.log("the value of the mode is " + modes);
   document.getElementById("mode").innerHTML = modes;
 }
 
+// Function to calculate the frequency of doubles
 function fdoubles() {
   var double = 0;
   if (dicenum == 2) {
     for (i = 0; i < results.length - 1; i += 2) {
-      number1 = results[i];
-      //console.log("the first number is " + number1);
-      number2 = results[i + 1];
-      // console.log("the 2nd number is " + number2);
-      if (number1 == number2) {
+      if (results[i] == results[i + 1]) {
         double++;
       }
     }
   }
   if (dicenum == 3) {
     for (i = 0; i < results.length - 1; i += 3) {
-      number1 = results[i];
-      // console.log("the first number is " + number1);
-      number2 = results[i + 1];
-      //console.log("the 2nd number is " + number2);
-      number3 = results[i + 2];
-      //console.log("the 3rd number is " + number3);
-      if (
-        (number1 == number2 && number1 != number3) ||
-        (number2 == number3 && number2 != number1) ||
-        (number1 == number3 && number1 != number2)
-      ) {
+      if ((results[i] == results[i + 1] && results[i] != results[i + 2]) ||
+        (results[i + 1] == results[i + 2] && results[i + 1] != results[i]) ||
+        (results[i] == results[i + 2] && results[i] != results[i + 1])) {
         double++;
       }
     }
   }
   document.getElementById("doubles").innerHTML = double;
 }
+
+// Function to calculate the frequency of triples
 function ftriples() {
   var triple = 0;
   if (dicenum == 3) {
     for (i = 0; i < results.length - 1; i += 3) {
-      number1 = results[i];
-      // console.log("the first number is " + number1);
-      number2 = results[i + 1];
-      //  console.log("the 2nd number is " + number2);
-      number3 = results[i + 2];
-      // console.log("the 3rd number is " + number3);
-      if (number1 == number2 && number1 == number3) {
-        triple = +1;
+      if (results[i] == results[i + 1] && results[i] == results[i + 2]) {
+        triple++;
       }
-
-      //  document.getElementById("demo").innerHTML = "Found " + x + " cells in the first tr";
-      // document.getElementById("triple").innerHTML = triple;
     }
   }
   document.getElementById("triples").innerHTML = triple;
 }
 
-//  for (i = 0; i < results.length; i += 1) {
-//if ((results[i] = results[i + 1]) && results[i] != results[i + 2]) {
-
-//  number= results[i];
-//  freqdouble.push(Number(i));
-//  document.getElementById("doubles").innerHTML = number;
-// document.getElementById("double").innerHTML = double;
-
+// Function to update frequency table for one die
 function fonetable() {
   if (numDice == 1) {
+    // Reset frequencies and modes frequencies arrays
     frequencies = [0, 0, 0, 0, 0, 0, 0];
     modesfreq = [0, 0, 0, 0, 0, 0, 0];
+    // Update frequency table
     for (var i = 0; i < 6; i++) {
       newRow = statsTable.insertRow();
       newCell = newRow.insertCell();
-
       newCell.innerHTML = "Frequency of  " + (i + 1);
-
       newCell = newRow.insertCell();
       newCell.innerHTML = " ";
     }
+    // Reset frequencies in the stats table
     for (i = 6; i < 12; i++) {
       var table = document.getElementById("statstable");
       if (table.rows[i].cells[1].innerHTML) {
         table.rows[i].cells[1].innerHTML = 0;
       }
     }
-
+    // Calculate frequencies
     for (i = 0; i < numRolls; i++) {
       var sum = results[i];
       for (x = 1; x < 7; x++) {
         if (sum == x) {
           frequencies[sum]++;
           modesfreq[sum]++;
-          //adds one to the frequency of that number
           table.rows[x + 5].cells[1].innerHTML = frequencies[sum];
         }
       }
     }
   }
 }
+
+// Function to update frequency table for two dice
 function ftwotable() {
-  //this array sets the value of the frequency for each number to zero
   if (numDice == 2) {
     frequencies = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     modesfreq = [0, 0, 0, 0, 0, 0, 0];
     for (var i = 1; i < 12; i++) {
-      //draws table for frequency
       newRow = statsTable.insertRow();
       newCell = newRow.insertCell();
-
       newCell.innerHTML = "Frequency of  " + (i + 1);
-
       newCell = newRow.insertCell();
       newCell.innerHTML = " ";
     }
-
     for (i = 6; i < 17; i++) {
       var table = document.getElementById("statstable");
-      //adds a zero to all frequencies as a base
       if (table.rows[i].cells[1].innerHTML) {
         table.rows[i].cells[1].innerHTML = 0;
       }
     }
-
     for (i = 0; i < results.length - 1; i += 2) {
-      //numrolls times two bc two dies are rolled, i is added by two bc freq is for every two numbers
-
       var sum = results[i] + results[i + 1];
-      //  console.log("the sum is " + sum);
-
       var table = document.getElementById("statstable");
-
       for (x = 2; x < 13; x++) {
         if (sum == x) {
           frequencies[sum]++;
-          //adds one to the frequency of that number
           table.rows[x + 4].cells[1].innerHTML = frequencies[sum];
         }
       }
     }
-    //modes frequency list
     for (i = 0; i < modesfreq.length; i++) {
       var num = results[i];
       for (x = 1; x < 7; x++) {
@@ -297,6 +238,8 @@ function ftwotable() {
     }
   }
 }
+
+// Function to update frequency table for three dice
 function fthreetable() {
   if (numDice == 3) {
     frequencies = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -304,9 +247,7 @@ function fthreetable() {
     for (var i = 2; i < 18; i++) {
       newRow = statsTable.insertRow();
       newCell = newRow.insertCell();
-
       newCell.innerHTML = "Frequency of  " + (i + 1);
-
       newCell = newRow.insertCell();
       newCell.innerHTML = " ";
     }
@@ -318,7 +259,6 @@ function fthreetable() {
     }
     for (i = 0; i < numRolls * 3; i += 3) {
       var num = results[i] + results[i + 1] + results[i + 2];
-      //  console.log("num is " + num);
       var table = document.getElementById("statstable");
     }
     for (i = 0; i < numRolls * 3; i += 3) {
@@ -326,13 +266,11 @@ function fthreetable() {
       for (x = 3; x < 19; x++) {
         if (sum == x) {
           frequencies[sum]++;
-          //adds one to the frequency of that number
           table.rows[x + 3].cells[1].innerHTML = frequencies[sum];
         }
       }
     }
     for (i = 0; i < modesfreq.length; i++) {
-      //   console.log("the initial freq is" + modesfreq);
       var num = results[i];
       for (x = 1; x < 7; x++) {
         if (num == x) {
@@ -341,32 +279,24 @@ function fthreetable() {
       }
     }
   }
-
-  console.log("the value of numdice is " + numDice);
 }
 
+// Function to handle rolling animation of the dice
 function run() {
-  // retrieve the element
   element = document.getElementById("dice");
-
-  // reset the transition by...
   document.getElementById("dice").addEventListener(
     "click",
     function (e) {
       e.preventDefault;
-
-      // removing the class
       element.classList.remove("rolldice");
-
       void element.offsetWidth;
-
-      //re-adding the class
       element.classList.add("rolldice");
     },
     false
   );
 }
 
+// Function to reset the form and tables
 function reset() {
   // Clear radio button selection
   var radioButtons = document.querySelectorAll('input[type="radio"]');
